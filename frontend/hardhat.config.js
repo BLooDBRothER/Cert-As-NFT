a4be15d4ef16ed7c8c665ca05eb8031b2b2bf117
Arul Murugavel <computertechnology446@gmail.com>
2023-03-19 18:42:40 +0530

    Add hardhat config and babelrc

---
+++ frontend/hardhat.config.js
@@ -0,0 +1,33 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+const { PRIVATE_KEY } = process.env;
+module.exports = {
+  defaultNetwork: "PolygonMumbai",
+  networks: {
+    hardhat: {
+    },
+    PolygonMumbai : {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [PRIVATE_KEY],
+      gasPrice: 8000000000
+    }
+  },
+  paths: {
+    artifacts: "./src/backend/artifacts",
+    sources: "./src/backend/contracts",
+    cache: "./src/backend/cache",
+    tests: "./src/backend/test"
+  },
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
